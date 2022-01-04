4cc12d1cdd1e7c1ddcfe2d0f300d3e89fac1cbda
AJ <ajvandewerken@gmail.com>
2022-01-04 19:18:10 +0100

    Update powermax.cpp

--- components/powermax/powermax.cpp
+++ components/powermax/powermax.cpp
@@ -7,6 +7,24 @@ namespace powermax {
 
 static const char *const TAG = "powermax";
 
+/*
+void setup() {
+   *     subscribe("the/topic", &MyCustomMQTTDevice::on_message);
+   *     pinMode(5, OUTPUT);
+   *   }
+   *
+   *   // topic and payload parameters can be removed if not needed
+   *   // e.g: void on_message() {
+   *
+   *   void on_message(const std::string &topic, const std::string &payload) {
+   *     // do something with topic and payload
+   *     if (payload == "ON") {
+   *       digitalWrite(5, HIGH);
+   *     } else {
+   *       digitalWrite(5, LOW);
+   *     }
+   *   }
+*/
 void PowerMaxAlarm::setup() {
   ESP_LOGD(TAG, "Setup");
 
