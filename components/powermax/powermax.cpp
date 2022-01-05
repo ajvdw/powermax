efb2844634c20cdf8eda678fc78b7482857b24fa
AJ <ajvandewerken@gmail.com>
2022-01-05 20:06:51 +0100

    Update powermax.cpp

--- components/powermax/powermax.cpp
+++ components/powermax/powermax.cpp
@@ -49,7 +49,7 @@ void PowerMaxDevice::loop() {
 void PowerMaxDevice::on_message(const std::string &topic, const std::string &payload) {
   // do something with topic and payload
   ESP_LOGD(TAG,"Payload %s on topic %s received",payload.c_str(), topic.c_str());
-  ESP_LOGD(TAG,"Prefix %s", this->get_command_topic_() );
+  ESP_LOGD(TAG,"Prefix %s", this->get_default_object_id_() );
 
   if (payload=="DISARM")
     this->sendCommand(Pmax_DISARM);  
