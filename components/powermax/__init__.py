e9d5867f1b07bf2a910f21c917cddb6d2fe6ba3e
AJ <ajvandewerken@gmail.com>
2022-01-05 12:33:32 +0100

    Update __init__.py

--- components/powermax/__init__.py
+++ components/powermax/__init__.py
@@ -1,4 +1,4 @@
-   import esphome.codegen as cg
+import esphome.codegen as cg
 import esphome.config_validation as cv
 
 from esphome.components import uart, mqtt
@@ -37,4 +37,4 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
-
+    
